83bcb1799ec733144ab9226a50482cc9e01d1cbb
DESKTOP-KREV8QC\Qavi Technologies <shahzadtayyab14015@gmail.com>
2026-04-24 15:23:12 +0200

    updated js snippet for latest site

--- app/layout.tsx
+++ app/layout.tsx
@@ -166,11 +166,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationJsonLd) }}
         />
 
-        <script
-          src="https://api.llmaffiliate.com/static/onboarding-snippet.js"
-          data-site-id="site_3a829f3dab43138212eaa5d132dbcae0"
-          data-token="llmafftm_963ee5a0021935a735b4bb884f1cfb88034f4b4981f71a20b4e4faf18607c07f"
-        ></script>
+<script
+  src="https://api.llmaffiliate.com/static/onboarding-snippet.js"
+  data-site-id="site_776f7d692376ae8aa9e0d5e368a8e3b5"
+  data-token="llmafftm_06afaa2ea288ff13785dceaa488264cc6e5d4267c1c71692f485f6b75c2d38bb">
+</script>
       </body>
     </html>
   );
