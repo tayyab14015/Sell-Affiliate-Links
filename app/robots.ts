b78af4b3cf2013f2cef89d5e19398fceb4ee5ad3
DESKTOP-KREV8QC\Qavi Technologies <shahzadtayyab14015@gmail.com>
2026-04-28 14:45:26 +0200

    updated robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -7,10 +7,50 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: '/'
+        allow: ['/', '/laptops']
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: ['/', '/laptops']
+      },
+      {
+        userAgent: 'OAI-SearchBot',
+        allow: ['/', '/laptops']
+      },
+      {
+        userAgent: 'ChatGPT-User',
+        allow: ['/', '/laptops']
+      },
+      {
+        userAgent: 'PerplexityBot',
+        allow: ['/', '/laptops']
+      },
+      {
+        userAgent: 'ClaudeBot',
+        allow: ['/', '/laptops']
+      },
+      {
+        userAgent: 'Google-Extended',
+        allow: ['/', '/laptops']
+      },
+      {
+        userAgent: 'Gemini',
+        allow: ['/', '/laptops']
+      },
+      {
+        userAgent: 'GoogleOther',
+        allow: ['/', '/laptops']
+      },
+      {
+        userAgent: 'GoogleOther-Image',
+        allow: ['/', '/laptops']
+      },
+      {
+        userAgent: 'GoogleOther-Video',
+        allow: ['/', '/laptops']
       }
     ],
-    sitemap: `${SITE_URL}/sitemap.xml`,
+    sitemap: [`${SITE_URL}/sitemap.xml`, '/sitemap.xml'],
     host: SITE_URL
   };
 }
