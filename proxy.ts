fc599f34b88c069a2ad60ca793dc77c4f84b0b79
tayyab <shahzadtayyab14015@gmail.com>
2026-08-28 15:23:10 +0200

    removed apex to www from code

--- proxy.ts
+++ proxy.ts
@@ -1,17 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { categoryToSlug } from './app/site';
 
-const APEX_HOST = 'aisneer.com';
-const CANONICAL_ORIGIN = 'https://www.aisneer.com';
-
 export function proxy(request: NextRequest) {
   const url = request.nextUrl;
-  const hostname = (request.headers.get('host') ?? '').split(':')[0].toLowerCase();
-
-  if (hostname === APEX_HOST) {
-    const dest = new URL(`${url.pathname}${url.search}`, CANONICAL_ORIGIN);
-    return NextResponse.redirect(dest, 301);
-  }
 
   if (
     url.pathname === '/laptops/dell-xp-13-9348' ||
