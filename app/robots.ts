c98015c617971c2d7782a859e8cb873c96a0fef3
tayyab <shahzadtayyab14015@gmail.com>
2026-09-16 13:20:58 +0200

    Revert "fixing and testing SEO for deepseek"
    This reverts commit eaf4dbb61a61ea1ffca204b5db23fad24ea468a1.

--- app/robots.ts
+++ app/robots.ts
@@ -35,9 +35,6 @@ const CRAWLERS = [
   'PerplexityBot',
   'Perplexity-User',
 
-  // DeepSeek (crawler identity is not officially documented; wildcard also allows it)
-  'DeepSeekBot',
-
   // Microsoft
   'Bingbot',
   'msnbot',
